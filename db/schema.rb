8b02544329625072bd1e2245d635ca7b94d44270
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-09 20:04:20 +0900

    devise_registrations_create

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_01_07_234125) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "family_name", default: "", null: false
