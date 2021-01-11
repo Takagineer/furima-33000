0faa096cb337058af7021b05a9edf95352750d5f
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-11 17:14:16 +0900

    テストコード＿メールアドレスが必須

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
  describe "ユーザー新規登録" do
    it "nicknameが空だと登録できない" do
-    user = User.new(nickname:"", email:"tanaka@tanaka", password: "tanaka10", password_confirmation:"tanaka10",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ",birthday:"1930")
+    user = User.new(nickname:"", email:"tanaka@tanaka", password: "tanaka10", password_confirmation:"tanaka10",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ")
     user.valid?
     expect(user.errors.full_messages).to include("Nickname can't be blank")
-
    end
-   it "nicknameが空だと登録できない" do
-   end
+ end
+   
+ describe "メールアドレスが必須であること" do
+  it "nicknameが空だと登録できない" do
+   user = User.new(nickname:"なかちゃん", email:"", password: "tanaka10", password_confirmation:"tanaka10",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ")
+   user.valid?
+   expect(user.errors.full_messages).to include("Email can't be blank") 
   end
+end
+  
+
 end
