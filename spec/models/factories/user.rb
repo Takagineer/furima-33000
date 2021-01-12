82e716dff52af8bb76d58d4b216b36861c127243
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-12 10:33:01 +0900

    テストコード_factorybot導入

---
+++ spec/models/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"なかちゃん"}
+    email                 {"tanak@tanaka"}
+    password              {"tanaka10"}
+    password_confirmation {password}
+    family_name           {田中}
+    last_name             {田仲}
+    family_name_kana      {タナカ}
+    last_name_kana        {タナカ}
+
+  end
+end
