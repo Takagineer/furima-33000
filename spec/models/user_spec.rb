8ce826aba65640d316fc4d8e360901969d73e238
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-12 10:24:28 +0900

    テストコード_passが必須_passが６文字以上

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,29 @@ RSpec.describe User, type: :model do
    end
  end
    
- describe "メールアドレスが必須であること" do
-  it "nicknameが空だと登録できない" do
+
+  it "emaileが空だと登録できない" do
    user = User.new(nickname:"なかちゃん", email:"", password: "tanaka10", password_confirmation:"tanaka10",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ")
    user.valid?
    expect(user.errors.full_messages).to include("Email can't be blank") 
   end
-end
+
   
+  it "パスワードが存在すること" do
+   user = User.new(nickname:"なかちゃん", email:"tanaka@tanaka", password: "", password_confirmation:"",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ")
+   user.valid?
+   expect(user.errors.full_messages).to include("Password can't be blank")
+  end
+
+
+  it "パスワードが６文字以上" do
+   user = User.new(nickname:"なかちゃん", email:"tanaka@tanaka", password: "iii22", password_confirmation:"iii22",family_name:"田中",last_name:"田仲",family_name_kana:"タナカ",last_name_kana:"タナカ")
+   user.valid?
+   expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+  end
+
+
+ 
+ 
 
 end
