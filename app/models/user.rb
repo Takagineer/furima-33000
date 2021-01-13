e7b7ce3a9b592ad371068234a0f3b9e969aa5a98
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-13 18:00:05 +0900

    バリデーション_with＿optionsによる記述省略

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+
+    validates :nickname           
+    validates :email              
+    validates :encrypted_password 
+    validates :birthday           
+
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+      validates :family_name        
+      validates :last_name          
+    end
+
+    with_options format: { with: /\A[ァ-ヶ]+\z/} do
+      validates :family_name_kana   
+      validates :last_name_kana     
+    end
   
-  validates :nickname           ,presence: true
-  validates :email              ,presence: true
-  validates :encrypted_password ,presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
-  validates :family_name        ,presence: true
-  validates :last_name          ,presence: true
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
-  validates :family_name_kana   ,presence: true
-  validates :last_name_kana     ,presence: true
-  end
-  validates :birthday           ,presence: true
-
-
-
+    
 end
+
+
+
