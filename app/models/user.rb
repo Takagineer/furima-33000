a5980291575c85e34cf9a80e6b90125b24316d8c
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-13 17:16:02 +0900

    テストコード_ユーザー本名の振り仮名は全角（カタカナ）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,11 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
   validates :family_name        ,presence: true
   validates :last_name          ,presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
   validates :family_name_kana   ,presence: true
   validates :last_name_kana     ,presence: true
-end
+  end
   validates :birthday           ,presence: true
 
 
