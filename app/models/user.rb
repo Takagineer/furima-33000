85fe3c67d198ebd542be0d996eb154bab9ea9dd2
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-13 19:16:55 +0900

    バリデーション修正_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   with_options presence: true do
 
     validates :nickname           
-    validates :email              
-    validates :encrypted_password 
+    validates :email
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\z/  
+    validates :password, length:{ minimum: 6}, format: { with: VALID_PASSWORD_REGEX}
     validates :birthday           
 
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
