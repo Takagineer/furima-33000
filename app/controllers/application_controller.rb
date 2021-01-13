00b5b662955c73426fdc68c1063ccd834f06fc34
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-13 18:52:46 +0900

    devise_ストロングパラメーター_encryptedpasswordの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name,:last_name,:family_name_kana,:last_name_kana,:birthday])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name,:last_name,:family_name_kana,:last_name_kana,:birthday])
 
   end
 
