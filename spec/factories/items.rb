a4cb73b417f90978389756d210276cbe3f6b4b52
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-14 21:52:11 +0900

    factorybotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
     
+    title                 {"ベルト"}
+    explanation           {"このベルトはバンダム級王者に送られるベルトです"}
+    category_id           {"3"}
+    status_id             {"2"}
+    shipping_cost_id      {"2"}
+    delivery_source_id    {"14"}
+    preparation_day_id    {"2"}
+    selling_price         {"50000"}
   end
 end
