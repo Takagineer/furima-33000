ca4b223c75e671708107b0e71d6c867501daf0f7
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-14 12:46:11 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+
+  validates :title
+  validates :explanation
+  validates :category_id
+  validates :status_id
+  validates :shipping_cost_id
+  validates :delivery_source_id
+  validates :preparation_day_id
+  validates :selling_price
+  validates :user_id
+
+  has_one_attached :image
+
 end
