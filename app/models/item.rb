69d3f86bfe463d1d97988b44fd7c3e290b03f790
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-14 16:02:20 +0900

    itemモデルバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,17 @@ class Item < ApplicationRecord
 
   with_options presence: true do
 
-  validates :title
-  validates :explanation
-  validates :category_id
-  validates :status_id
-  validates :shipping_cost_id
-  validates :delivery_source_id
-  validates :preparation_day_id
-  validates :selling_price
-  validates :user_id
-
+    validates :title
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :delivery_source_id
+    validates :preparation_day_id
+    validates :selling_price
+    validates :user_id
+  end
+  
   has_one_attached :image
   belongs_to_active_hash :genre
 
