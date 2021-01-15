b80431bea05d993527a7d23400587b9a8ecf417d
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-15 12:07:46 +0900

    itemモデル_テストコード準備

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "商品出品機能" do
+    # 正常系テストコード
+    context "商品出品できる時" do
+      it '全項目入力されていれば保存できる' do
+        @item.save
+        @item.valid?
+        binding.pry
+      end
+    end
+
+    # 異常系テストコード
+    contect "商品出品できない時" do
+      it '' do
+      end
+    end
+  end
 end
