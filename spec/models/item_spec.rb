35f53f1e997a8b1cd354a56b1fb2e8e552fb6792
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-15 15:01:47 +0900

    itemモデル_異常系テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,9 +15,55 @@ RSpec.describe Item, type: :model do
     end
 
     # 異常系テストコード
-    contect "商品出品できない時" do
-      it '' do
+    context "商品出品できない時" do
+      it '商品名が必須であること' do
+        @item.title = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Title can't be blank"
       end
+
+      it '商品の説明が必須であること' do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Explanation can't be blank"
+      end
+
+      it 'カテゴリーの情報が必須であること' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+
+      it '商品の状態についての情報が必須であること' do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status can't be blank"
+      end
+
+      it '配送料の負担についての情報が必須であること' do
+        @item.shipping_cost_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping cost can't be blank"
+      end
+
+      it '発送元の地域についての情報が必須であること' do
+        @item.delivery_source_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery source can't be blank"
+      end
+
+      it '発送までの日数についての情報が必須であること' do
+        @item.preparation_day_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Preparation day can't be blank"
+      end
+
+      it '価格についての情報が必須であること' do
+        @item.selling_price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Selling price can't be blank"
+      end
+
     end
   end
 end
