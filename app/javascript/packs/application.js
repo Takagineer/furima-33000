c965aa5e9111fac1cb7f0b79643714cc7abe8c44
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-17 16:07:32 +0900

    javascriptファイル記述の修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,7 @@ require("@rails/ujs").start()
 
 require("@rails/activestorage").start()
 require("channels")
-require("channels")
-require("app/javascript/item_price.js")
+require("item_price.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
