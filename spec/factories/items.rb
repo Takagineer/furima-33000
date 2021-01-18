69de726034c1aed54eb2f8d3e0e27c4181a084c9
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-18 19:25:32 +0900

    エラーメッセージの表示とFactoryBotのクオテーションマークの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     
     title                 {"ベルト"}
     explanation           {"このベルトはバンダム級王者に送られるベルトです"}
-    category_id           {"3"}
-    status_id             {"2"}
-    shipping_cost_id      {"2"}
-    delivery_source_id    {"14"}
-    preparation_day_id    {"2"}
-    selling_price         {"50000"}
-    user_id               {"100"}
+    category_id           {3}
+    status_id             {2}
+    shipping_cost_id      {2}
+    delivery_source_id    {14}
+    preparation_day_id    {2}
+    selling_price         {50000}
+    user_id               {100}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
