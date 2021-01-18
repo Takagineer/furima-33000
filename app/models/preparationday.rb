29503bba1118ea637ffc1d58c2230979a553a913
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-18 17:23:24 +0900

    商品出品ページ_form_withのurlとFactoruBotの微調整

--- app/models/preparationday.rb
+++ app/models/preparationday.rb
@@ -1,6 +1,6 @@
 class Preparationday < ActiveHash::Base
   self.data =[
-    {id: 1, name: '---'}, {id: 2, name: '1~2日'}, {id: 3, name: '送料込み(出品者負担)'}
+    {id: 1, name: '---'}, {id: 2, name: '1~2日で発送'}, {id: 3, name: '2~3日で発送'}, {id: 4, name: '4~7日で発送'}
   ]
 
   include ActiveHash::Associations
