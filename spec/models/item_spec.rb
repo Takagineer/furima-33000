3447313c9acd1c2bc72975e3f45baaec0899eb50
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-19 11:59:22 +0900

    テストコード_文字列を数値に

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,37 +35,37 @@ RSpec.describe Item, type: :model do
       end
 
       it '商品の状態についての情報が必須であること' do
-        @item.status_id = "1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Status must be other than 1"
       end
 
       it '配送料の負担についての情報が必須であること' do
-        @item.shipping_cost_id = "1"
+        @item.shipping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping cost must be other than 1"
       end
 
       it '発送元の地域についての情報が必須であること' do
-        @item.delivery_source_id = "1"
+        @item.delivery_source_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Delivery source must be other than 1"
       end
 
       it '発送までの日数についての情報が必須であること' do
-        @item.preparation_day_id = "1"
+        @item.preparation_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include "Preparation day must be other than 1"
       end
 
       it '価格が299以下の時' do
-        @item.selling_price = "299"
+        @item.selling_price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Selling price is not included in the list"
       end
 
       it '価格が10000000以上の時' do
-        @item.selling_price = "10000000"
+        @item.selling_price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include "Selling price is not included in the list"
       end
