7d338f219d38350a9786095be4167a53ad63366a
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-25 19:41:41 +0900

    商品購入機能＿新しいモデルの記述

---
+++ app/models/purchase_order.rb
@@ -0,0 +1,7 @@
+class PurchaseOrder
+  include ActiveModel::Model
+  attr_accessor :posta;_code,:prefecture_id,:city,:address,:building_name,:phone_number,:order_id,:user_id,:item_id
+
+  validates
+  
+end
