3064a0ce54f3b018d39d9241ee44d513b2b2b506
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-25 20:37:14 +0900

    バリデーションの記述

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,7 +1,22 @@
-class PurchaseOrder
+class PurchaseOrder 
   include ActiveModel::Model
-  attr_accessor :posta;_code,:prefecture_id,:city,:address,:building_name,:phone_number,:order_id,:user_id,:item_id
+  attr_accesso :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
 
-  validates
-  
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "郵便番号エラー"}
+    validates :prefecture_id, format: { with: numericality: {other_than: 0}}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "町名記入"}
+    validates :address, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "住所記入"}
+    validates :phone_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: "電話番号を記入"}
+    validates :order_id, format: { with: numericality: {other_than: 0}}
+    validates :user_id, format: { with: numericality: {other_than: 0}}
+    validates :item_id, format: { with: numericality: {other_than: 0}}
+  end
+
+  def save
+    
+  end
 end
+
+
+numericality: { other_than: 1}
