152032a9cc94d637d4a741108eb66daa1895dcae
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-26 16:20:48 +0900

    連結モデルファイル_attr_accessorの記述修正

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,6 +1,6 @@
 class PurchaseOrder 
   include ActiveModel::Model
-  attr_accesso :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "郵便番号エラー"}
