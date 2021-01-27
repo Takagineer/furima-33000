78499b22ba5afc9c21769a8defcba32ca86ec54f
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-27 16:49:00 +0900

    コントローラー内renderの記述とバリデーションの記述修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,17 +7,18 @@ class OrdersController < ApplicationController
 
   def create
     @purchase_order = PurchaseOrder.new(purchase_order_params)
-     if @purchase_order.save.valid?
+     if @purchase_order.valid?
           @purchase_order.save
-          redirect_to action::index
+          redirect_to root_path
      else
-       render action::new
+      @purchase_orders = Item.find(params[:item_id])
+      render :index
      end
   end
 
   private  
    def purchase_order_params
-    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
+    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id, item_id: Item.find(params[:item_id]).id)
   end
 end
 
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -7,17 +7,17 @@ class PurchaseOrder
     validates :prefecture_id, numericality: { other_than: 1, message: '県指定'}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "町名記入"}
     validates :address, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "住所記入"}
-    validates :phone_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: "電話番号を記入"}
-    validates :order_id, presence: true
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: "電話番号を記入"}
     validates :user_id, presence: true
     validates :item_id, presence: true
   end
 
   def save
-    Order.create(user_id: user_id,item_id: item_id)
-    Purchase.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order_id)
+    value = Order.create(user_id: user_id,item_id: item_id)
+    Purchase.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: value.id)
 
   end
 end
+
 
 
