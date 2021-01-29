3e76c35f30bb8bc2dd1ffef19958c6cbef4d9d22
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-29 16:18:21 +0900

    トークンの取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,27 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_9508c4e1180fc0ea892949c5");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    // カード情報を変数cardに格納
+    const card = {
+      number: formData.get("purchase_order[card-number]"),
+      cvc: formData.get("purchase_order[card-cvc]"),
+      exp_month: formData.get("purchase_order[card-exp-month]"),
+      exp_year: `20${formData.get("purchase_order[card-exp-year]")}`,
+    };
+    console.log(card)
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200){
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
