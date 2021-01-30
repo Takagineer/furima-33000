abed10917dc23f4aef670b8574b793da57cd4f7b
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-30 13:37:47 +0900

    出品者がURLから直接商品購入ページに移動できないようにする機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, only:[:index]
+
   def index
     @purchase_order = PurchaseOrder.new
     @purchase_orders = Item.find(params[:item_id])
@@ -23,13 +25,18 @@ class OrdersController < ApplicationController
     params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id, item_id: Item.find(params[:item_id]).id, token: params[:token])
    end
 
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-          amount: @purchase_orders.selling_price,
-          card: purchase_order_params[:token],
-          currency: 'jpy'
-        )
+    def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+            amount: @purchase_orders.selling_price,
+            card: purchase_order_params[:token],
+            currency: 'jpy'
+          )
+    end
+
+    def move_to_index
+      if current_user == Item.find(params[:item_id]).user
+        redirect_to root_path
+    end
   end
 end
-
