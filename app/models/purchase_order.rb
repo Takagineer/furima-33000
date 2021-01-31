0037758f8c50f8a4ba3c103fa08c0cdf67142daf
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-01-31 19:49:23 +0900

    バリデーションの記述修正

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,9 +3,9 @@ class PurchaseOrder
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "can't be blank"}
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select'}
-    validates :city, :address, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "can't be blank"}
+    validates :city, :address, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Phone number input only number"}
     validates :user_id
     validates :item_id
